6630a15a9e91706cf58e08e78b4d88227b596fae
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-12-24 16:06:03 +0000

    Import

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+import getpass
+import pyperclip
+
+print(r)
